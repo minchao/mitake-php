79f4c76a1d1dda0c782e7b417aadc6f0b1889a6b
minchao <minchao.220@gmail.com>
2017-10-01 16:44:39 +0800

    Add ResponseTest

---
+++ tests/Message/ResponseTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Mitake\Tests\Message;
+
+use Mitake\Message\Response;
+use Mitake\Message\Result;
+use Mitake\Message\StatusCode;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ResponseTest
+ * @package Mitake\Tests\Message
+ */
+class ResponseTest extends TestCase
+{
+    /**
+     * @return Response
+     */
+    public function testConstruct()
+    {
+        $r = (new Result())
+            ->setMsgid('1234567890')
+            ->setStatuscode(new StatusCode('4'));
+        $resp = (new Response())
+            ->addResult($r)
+            ->setAccountPoint(99);
+
+        $this->assertEquals([$r], $resp->getResults());
+
+        return $resp;
+    }
+
+    /**
+     * @depends testConstruct
+     * @param Response $obj
+     */
+    public function testToArray($obj)
+    {
+        $expected = [
+            'results' => [
+                [
+                    'msgid' => '1234567890',
+                    'statuscode' => '4',
+                ],
+            ],
+            'accountPoint' => 99,
+        ];
+
+        $this->assertEquals($expected, $obj->toArray());
+    }
+}
